733bfcc42cf20b999d027997f2f3d04b24a92391
Brandon Powell <bpowell29a@gmail.com>
2013-09-26 21:22:59 -0400

    Sprite class now loads image.

--- src/Sprite.cpp
+++ src/Sprite.cpp
@@ -3,5 +3,18 @@
 namespace dngine{
 	Sprite::Sprite(std::string fname){
 		filename = fname;
+		image = load_image(filename);
+	}
+
+	SDL_Surface* Sprite::load_image(std::string fname){
+		SDL_Surface *loaded = NULL, *optimized = NULL;
+		loaded = IMG_Load(fname.c_str());
+
+		if(loaded!=NULL){
+			optimized = SDL_DisplayFormatAlpha(loaded);
+			SDL_FreeSurface(loaded);
+		}
+
+		return optimized;
 	}
 }
--- src/include/Sprite.h
+++ src/include/Sprite.h
@@ -3,12 +3,14 @@
 
 #include <string>
 #include "SDL/SDL.h"
+#include "SDL/SDL_image.h"
 
 namespace dngine{
 	class Sprite{
 		private:
 			std::string filename;
 			SDL_Surface *image;
+			SDL_Surface *load_image(std::string fname);
 		public:
 			Sprite(std::string fname);
 			void render();
