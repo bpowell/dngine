ef84cc70441d3a853bb8cd7fd05f3ff2854c3735
Brandon Powell <bpowell29a@gmail.com>
2014-12-06 22:20:53 -0500

    added global header file

---
+++ src/include/global.h
@@ -0,0 +1,11 @@
+#ifndef _GLOBAL_H_
+#define _GLOBAL_H_
+
+#include <SDL2/SDL2.h>
+#include <memory>
+
+typedef std::shared_ptr<SDL_Window> SDL_Window_ptr;
+typedef std::shared_ptr<SDL_Renderer> SDL_Renderer_ptr;
+typedef std::shared_ptr<SDL_Texture> SDL_Texture_ptr;
+
+#endif
