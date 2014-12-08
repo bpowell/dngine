ee0fe464e0a17048462b938fccc340df6b612338
Brandon Powell <bpowell29a@gmail.com>
2014-12-07 21:01:02 -0500

    added rect sharedptr and creator

--- src/include/global.h
+++ src/include/global.h
@@ -7,5 +7,16 @@
 typedef std::shared_ptr<SDL_Window> SDL_Window_ptr;
 typedef std::shared_ptr<SDL_Renderer> SDL_Renderer_ptr;
 typedef std::shared_ptr<SDL_Texture> SDL_Texture_ptr;
+typedef std::shared_ptr<SDL_Rect> SDL_Rect_ptr;
+
+static SDL_Rect_ptr SDL_RECT(int x, int y, int w, int h) {
+    SDL_Rect_ptr rect = SDL_Rect_ptr(new SDL_Rect());
+    rect->x=x;
+    rect->y=y;
+    rect->w=w;
+    rect->h=h;
+
+    return rect;
+}
 
 #endif
